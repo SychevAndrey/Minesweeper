5f6faf239fa5480a7067e6843bcfa0f7dd36c8b2
Andrey S <nukemoore@gmail.com>
2020-08-22 13:33:30 +0300

    mini sell improvements

--- src/components/Minesweeper/Cell.tsx
+++ src/components/Minesweeper/Cell.tsx
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import styles from './index.module.scss'
 
 interface ICellProps {
-  value: string | number
+  value: number
 }
 
 const Cell: React.FC<ICellProps> = ({value}) => {
+
+  const [state, setState] = useState(false);
+  function mouseClickHandler(event: React.MouseEvent): void {
+
+    if (event) {
+      console.log(event)
+    }
+    setState(true);
+  }
+
   return (
-    <span className={styles.cell}>
-      {value}
+    <span onClick={(event) => mouseClickHandler(event)} className={styles.cell}>
+      {state ? value === 9 ? <span>B</span> : value : ''}
     </span>
   )
 }
--- src/components/Minesweeper/index.tsx
+++ src/components/Minesweeper/index.tsx
@@ -13,13 +13,15 @@ interface IMinesweeperProps {
 const Minesweeper: React.FC<IMinesweeperProps> = ({size, difficulty}) => {
 
     const cells = (data: Array<Array<number>>): JSX.Element[][] => {
-      return data.map(row => row.map(cell => <Cell value={cell} />))
+      return data.map((row) => row.map((cell) => <Cell value={cell} key={Math.random()} />))
     }
 
   const data: Array<Array<number>> = [[0, 0, 1, 9],
   [1, 1, 1, 1],
   [9, 2, 1, 0],
   [2, 9, 1, 0]];
+
+  console.log('render');
 
   return (
     <div style={{width: `${size*20}px`}} className={styles.field}>
